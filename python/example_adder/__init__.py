f68e1f8d8e3c14b644be6a43b76e1863f6f044a7
Mansard <nmansard@laas.fr>
2022-05-17 22:48:37 +0200

    [python] python library with _pywrap suffix to avoid cmake target conflicts.

--- python/example_adder/__init__.py
+++ python/example_adder/__init__.py
@@ -1,3 +1,3 @@
 import crocoddyl
-from .example_adder import add, adds, sub, subs  # noqa: F401
-from .example_adder import ActionModelUniEx  # noqa: F401
+from .example_adder_pywrap import add, adds, sub, subs  # noqa: F401
+from .example_adder_pywrap import ActionModelUniEx  # noqa: F401
--- python/main.cpp
+++ python/main.cpp
@@ -1,6 +1,6 @@
 #include "example-adder/python.hpp"
 
-BOOST_PYTHON_MODULE(example_adder) {
+BOOST_PYTHON_MODULE(example_adder_pywrap) {
   gepetto::example::exposeExampleAdder();
   gepetto::example::exposeExamplePin();
   gepetto::example::exposeActionUniEx();
