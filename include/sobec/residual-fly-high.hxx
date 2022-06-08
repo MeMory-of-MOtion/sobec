1b3f1c41acfb1452e6268c64618c04ac7d477616
Mansard <nmansard@laas.fr>
2022-06-08 22:58:43 +0200

    Clean version, working.

--- include/sobec/residual-fly-high.hxx
+++ include/sobec/residual-fly-high.hxx
@@ -44,13 +44,10 @@ void ResidualModelFlyHighTpl<Scalar>::calc(
 
   pinocchio::updateFramePlacement(pin_model_,*d->pinocchio,frame_id);
   data->r = pinocchio::getFrameVelocity(pin_model_,*d->pinocchio,
-                                //frame_id,pinocchio::LOCAL).linear().head(2);
                                 frame_id,pinocchio::LOCAL_WORLD_ALIGNED).linear().head(2);
-  //data->r.fill(1);
   d->ez = exp(-d->pinocchio->oMf[frame_id].translation()[2]/2);
-  //d->ez = d->pinocchio->oMf[frame_id].translation()[2];
-  //d->ez=1;
   data->r *= d->ez;
+
 }
 
 template <typename Scalar>
@@ -79,33 +76,27 @@ void ResidualModelFlyHighTpl<Scalar>::calcDiff(
    *      v' = R l_v' + R' l_v = R l_v' - l_v x Jr
    * Then r' = v'/e - r/2 z' = R l_v'/e - l_v x Jr/e - r/2 z'
    */
+
   pinocchio::getFrameVelocityDerivatives(pin_model_, *d->pinocchio, frame_id,
-                                         pinocchio::LOCAL, d->d_dq,d->d_dv);
-  //pinocchio::LOCAL_WORLD_ALIGNED, d->d_dq,d->d_dv);
+                                         pinocchio::LOCAL, d->l_dnu_dq,d->l_dnu_dv);
+  const Vector3s & v = pinocchio::getFrameVelocity(pin_model_,*d->pinocchio,
+                                                   frame_id,pinocchio::LOCAL).linear();
+  const Matrix3s & R = d->pinocchio->oMf[frame_id].rotation();
 
-
-  Vector3s v = pinocchio::getFrameVelocity(pin_model_,*d->pinocchio,
-                                           frame_id,pinocchio::LOCAL).linear();
-  MatrixXs R = d->pinocchio->oMf[frame_id].rotation();
-  MatrixXs l_dv_dq = d->d_dq.template topRows<3>();
-  MatrixXs l_dv_dv = d->d_dv.template topRows<3>();
-  MatrixXs l_Jw = d->d_dv.template bottomRows<3>();
-  MatrixXs o_d_dq = R*(l_dv_dq - pinocchio::skew(v)*l_Jw);
-  MatrixXs o_d_dv = (R*l_dv_dv);
-  
-  data->Rx.leftCols(nv) = o_d_dq.template topRows<2>();
-  data->Rx.rightCols(nv) = o_d_dv.template topRows<2>();
+  // First compute LWA derivatives of the velocity
+  d->vxJ.noalias() = pinocchio::skew(-v)*d->l_dnu_dv.template bottomRows<3>();
+  d->vxJ += d->l_dnu_dq.template topRows<3>();
+  d->o_dv_dq = R*d->vxJ;
+  d->o_dv_dv = R*d->l_dnu_dv.template topRows<3>();
+    
+  // First term with derivative of v
+  data->Rx.leftCols(nv) = d->o_dv_dq.template topRows<2>();
+  data->Rx.rightCols(nv) = d->o_dv_dv.template topRows<2>();
   data->Rx *= d->ez;
-  //data->Rx.fill(0);
-  //VectorXs v = pinocchio::getFrameVelocity(pin_model_,*d->pinocchio,
-  //                                         frame_id,pinocchio::LOCAL_WORLD_ALIGNED).linear();
-  pinocchio::getFrameJacobian(pin_model_, *d->pinocchio, frame_id,
-                              pinocchio::LOCAL_WORLD_ALIGNED, d->d_dq);
-  data->Rx.leftCols(nv).row(0) -= data->r[0]/2 * d->d_dq.row(2);
-  data->Rx.leftCols(nv).row(1) -= data->r[1]/2 * d->d_dq.row(2);
-  //data->Rx.leftCols(nv).row(0) +=  d->d_dq.row(2);
-  //data->Rx.leftCols(nv).row(1) +=  d->d_dq.row(2);
 
+  // Second term with derivative of z
+  data->Rx.leftCols(nv).row(0) -= data->r[0]/2 * d->o_dv_dv.row(2); //d->d_dq.row(2);
+  data->Rx.leftCols(nv).row(1) -= data->r[1]/2 * d->o_dv_dv.row(2); // d->d_dq.row(2);
 }
 
 template <typename Scalar>
