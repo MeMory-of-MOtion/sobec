a457d6fe3698bd648ad92ee98ed914c459c58014
Mansard <nmansard@laas.fr>
2022-06-14 19:38:48 +0200

    Fix infinite loop in test_mpc_walk.py

--- tests/python/test_mpc_walk.py
+++ tests/python/test_mpc_walk.py
@@ -13,7 +13,9 @@ Tmpc = 20
 
 robot = robex.load("talos_legs")
 model = robot.model
-cid = 16
+contactIds = [
+    i for i, f in enumerate(model.frames) if "sole_link" in f.name
+]
 
 runmodels = []
 
@@ -22,8 +24,9 @@ actuation = croc.ActuationModelFloatingBase(state)
 
 # Contact
 contacts = croc.ContactModelMultiple(state, actuation.nu)
-contact = croc.ContactModel6D(state, cid, pin.SE3.Identity(), actuation.nu, np.zeros(2))
-contacts.addContact("contact", contact)
+for cid in contactIds:
+    contact = croc.ContactModel6D(state, cid, pin.SE3.Identity(), actuation.nu,np.zeros(2))
+    contacts.addContact('contact'+str(cid),contact)
 
 # Costs
 costs = croc.CostModelSum(state, actuation.nu)
@@ -33,7 +36,6 @@ xRegResidual = croc.ResidualModelState(state, model.x0, actuation.nu)
 xRegCost = croc.CostModelResidual(state, xRegResidual)
 costs.addCost("stateReg", xRegCost, 1)
 
-
 damodel = croc.DifferentialActionModelContactFwdDynamics(
     state, actuation, contacts, costs, 1e-3, True
 )
@@ -41,7 +43,13 @@ amodel = croc.IntegratedActionModelEuler(damodel, 0.1)
 
 problem = croc.ShootingProblem(model.x0, [amodel for t in range(T)], amodel)
 ddp = croc.SolverFDDP(problem)
-ddp.solve()
+
+x0s = [problem.x0.copy() for _ in range(problem.T + 1)]
+u0s = [
+    m.quasiStatic(d, x)
+    for m, d, x in zip(problem.runningModels, problem.runningDatas, x0s)
+]
+ddp.solve(x0s,u0s)
 
 mpc = sobec.MPCWalk(problem)
 mpc.Tmpc = Tmpc
@@ -52,7 +60,6 @@ mpc.Tend = Tend
 mpc.vcomRef = np.array([0.33, 0.1, 0.0])
 mpc.initialize([x for x in ddp.xs[: Tmpc + 1]], [u for u in ddp.us[:Tmpc]])
 
-mpc.calc(model.x0, 10)
-mpc.solver.setCallbacks([croc.CallbackVerbose()])
-mpc.solver.solve()
-mpc.calc(model.x0, 10)
+mpc.calc(model.x0,10)
+mpc.solver.setCallbacks([croc.CallbackVerbose() ])
+mpc.calc(model.x0,10)
