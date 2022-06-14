b585a62ac3902110a9ac5545bf373dc96eb69f04
Mansard <nmansard@laas.fr>
2022-06-14 18:19:04 +0200

    Add test of mpc in complete scenario.

---
+++ python/sobec/walk/config_mpc.py
@@ -0,0 +1,14 @@
+
+
+def configureMPCWalk(mpc,params):
+    mpc.Tmpc = params.Tmpc
+    mpc.Tstart = params.Tstart
+    mpc.Tdouble = params.Tdouble
+    mpc.Tsingle = params.Tsingle
+    mpc.Tend = params.Tend
+    mpc.DT = params.DT
+    mpc.solver_th_stop = params.solver_th_stop
+    mpc.vcomRef = params.vcomRef
+    mpc.solver_reg_min = params.solver_reg_min
+    mpc.solver_maxiter = params.solver_maxiter
+
