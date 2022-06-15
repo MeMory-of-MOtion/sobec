a464c5e523825797cfdb78428878522b1d0daabd
Mansard <nmansard@laas.fr>
2022-06-15 11:54:39 +0200

    Add missing files.

---
+++ tests/python/test_ocpcpp.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.linalg import norm
+import example_robot_data as robex
+import sobec
+from sobec.walk.robot_wrapper import RobotWrapper as pyRobotWrapper
+
+urdf = robex.load("talos_legs")
+pyrobot = pyRobotWrapper(urdf.model, contactKey="sole_link")
+
+
+robot = sobec.OCPRobotWrapper(urdf.model,"sole_line","half_sitting")
+robot.model
+assert(norm(pyrobot.com0-robot.com0)<1e-9)
+assert(norm(pyrobot.x0-robot.x0)<1e-9)
+
+params = sobec.OCPWalkParams()
+params.DT
+
+ocp = sobec.OCPWalk(robot,params)
+ocp.problem
