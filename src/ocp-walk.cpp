b59c9a8bcff47a07292706cf3bd09f8bef003e58
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-06-15 14:35:55 +0200

    split ocp-walk.cpp

--- src/ocp-walk.cpp
+++ src/ocp-walk.cpp
@@ -14,44 +14,6 @@
 
 namespace sobec {
 
-OCPRobotWrapper::OCPRobotWrapper(boost::shared_ptr<pinocchio::Model> model_,
-                                 const std::string& contactKey,
-                                 const std::string& referencePosture) {
-  this->model = model_;
-
-  // Search contact Ids using key name (eg all frames containing "sole_link")
-  for (pinocchio::FrameIndex idx = 0; idx < model->frames.size(); ++idx) {
-    if (model->frames[idx].name.find(contactKey) != std::string::npos) {
-      std::cout << "Found contact " << idx << std::endl;
-      contactIds.push_back(idx);
-    }
-  }
-
-  // Add tow and heel frames ... TODO
-  for (pinocchio::FrameIndex cid : contactIds) {
-    towIds[cid] = cid;
-    heelIds[cid] = cid;
-  }
-
-  //   boost::shared_ptr<pinocchio::Data> data;
-  data = boost::make_shared<pinocchio::Data>(*model);
-
-  // Get ref config
-  Eigen::VectorXd q0 = model->referenceConfigurations[referencePosture];
-
-  // eval x0
-  x0.resize(model->nq + model->nv);
-  x0.head(model->nq) = q0;
-  x0.tail(model->nv).fill(0);
-
-  // eval COM
-  com0 = pinocchio::centerOfMass(*model, *data, q0, false);
-
-  // eval mass
-  robotGravityForce =
-      pinocchio::computeTotalMass(*model) * model->gravity.linear()[2];
-}
-
 boost::shared_ptr<IntegratedActionModelEuler> OCPWalk::buildTerminalModel() {
   return NULL;
 }
