f36c196a318ef23193d591a7b2650c26d86bcf0a
Mansard <nmansard@laas.fr>
2022-06-17 10:30:22 +0200

    ivigit read/write params file with yaml in py.

---
+++ python/sobec/walk/yaml_params.py
@@ -0,0 +1,100 @@
+'''
+This module provides functions to write paams into a yamlfile. Params can be either python params from
+sobec.walk.params, or c++ params from sobec.OCPWalkParams
+- yamlWriteParams
+
+'''
+
+import yaml
+import numpy as np
+import sobec
+
+# ################################################################################
+# ################################################################################
+# ################################################################################
+
+def paramsToDict(params):
+    res = params.__dict__
+    for k in params.__class__.__dict__.keys():
+        if k[:2]!='__' and k not in res:
+            res[k]=params.__getattribute__(k)
+    if len(params.__class__.__bases__)>0:
+        for k in params.__class__.__bases__[0].__dict__.keys():
+            if k[:2]!='__' and k not in res:
+                res[k]=params.__getattribute__(k)
+    return res
+    
+def flattenDictArrayValues(paramsAsDict):
+    '''
+    Taking a dict containing array vector values, flatten them to list
+    '''
+    for k,v in paramsAsDict.items():
+        if isinstance(v,np.ndarray):
+            paramsAsDict[k] = v.tolist()
+        if isinstance(v,sobec.sobec_pywrap.StdVectorStdStringIndex_):
+            paramsAsDict[k] = [str(vi) for vi in v]
+
+def yamlWrite(paramsAsDict,filename):
+    with open(filename, 'w') as outfile:
+        yaml.dump(paramsAsDict, outfile, default_flow_style=None)
+    
+
+def yamlWriteParams(filename,*args):
+    #pdict = { **paramsToDict(params) for params in args }
+    pdict = { k:v for params in args for k,v in paramsToDict(params).items() }
+    flattenDictArrayValues(pdict)
+    yamlWrite(pdict,filename)
+
+# ################################################################################
+# ################################################################################
+# ################################################################################
+
+def yamlRead(filename):
+    with open(filename, 'r') as infile:
+        return yaml.load(infile)
+
+def dictToNpValue(paramsAsDict):
+    for k,v in paramsAsDict.items():
+        if isinstance(v,list) and len(v)>0:
+            if isinstance(v[0],float) or isinstance(v[0],int):
+                paramsAsDict[k] = np.array(v,np.float64)
+    
+def yamlReadToParams(filename,params):
+    pdict = yamlRead(filename)
+    dictToNpValue(pdict)
+    for k,v in pdict.items():
+        if hasattr(params,k):
+            params.__setattr__(k,v)
+    
+# ################################################################################
+# ################################################################################
+# ################################################################################
+        
+if __name__ == "__main__":
+    import sobec.walk.params
+    params=sobec.walk.params.WalkParams('talos_low')
+    pdict = paramsToDict(params)
+    flattenDictArrayValues(pdict)
+    yamlWrite(pdict,'/tmp/walk.yaml')
+
+    ocpparams = sobec.OCPWalkParams()
+    ocpdict = paramsToDict(ocpparams)
+    mpcparams = sobec.MPCWalkParams()
+    mpcdict = paramsToDict(mpcparams)
+    cppdict = { k:v for params in [ocpparams,mpcparams] for k,v in paramsToDict(params).items() }
+    flattenDictArrayValues(cppdict)
+
+    yamlWriteParams('/tmp/swp_full.yml',params)
+    yamlWriteParams('/tmp/sobec_full.yml',ocpparams,mpcparams)
+
+    yamlReadToParams('/tmp/walk.yaml',ocpparams)
+    yamlReadToParams('/tmp/walk.yaml',mpcparams)
+
+    for paramClass in [sobec.OCPWalkParams, sobec.MPCWalkParams]:
+        for k in paramClass.__class__.__dict__.keys():
+            if k[:2]=='__': continue
+            if isinstance(ocpparams[k],float) or isinstance(ocpparams[k],int):
+                assert(abs(ocpparams[k]-params[k])<1e-6)
+            elif isinstance(ocpparams[k],np.ndarray):
+                assert(np.linalg.norm(ocpparams[k]-params[k])<1e-6)
+            
