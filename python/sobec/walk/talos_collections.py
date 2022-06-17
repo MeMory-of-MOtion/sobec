49a54928692144b23e60ccedbb5b28a8694e9890
Mansard <nmansard@laas.fr>
2022-06-17 09:19:17 +0200

    Add pyrene description.

--- python/sobec/walk/talos_collections.py
+++ python/sobec/walk/talos_collections.py
@@ -51,12 +51,44 @@ jointToLockCollection = {
         "torso_1_joint",
         "torso_2_joint",
     ],
-}
-
-
+    'pyrene_legs': [
+        # "universe",
+        "arm_left_1_joint",
+        "arm_left_2_joint",
+        "arm_left_3_joint",
+        "arm_left_4_joint",
+        "arm_left_5_joint",
+        "arm_left_6_joint",
+        "arm_left_7_joint",
+        "arm_right_1_joint",
+        "arm_right_2_joint",
+        "arm_right_3_joint",
+        "arm_right_4_joint",
+        "arm_right_5_joint",
+        "arm_right_6_joint",
+        "arm_right_7_joint",
+        "gripper_left_joint",
+        "gripper_right_joint",
+        "head_1_joint",
+        "head_2_joint",
+        "torso_1_joint",
+        "torso_2_joint",
+        'gripper_left_inner_double_joint',
+        'gripper_left_fingertip_1_joint',
+        'gripper_left_fingertip_2_joint',
+        'gripper_left_inner_single_joint',
+        'gripper_left_fingertip_3_joint',
+        'gripper_left_motor_single_joint',
+        'gripper_right_inner_double_joint',
+        'gripper_right_fingertip_1_joint',
+        'gripper_right_fingertip_2_joint',
+        'gripper_right_inner_single_joint',
+        'gripper_right_fingertip_3_joint',
+        'gripper_right_motor_single_joint'
+    ],
+    }
 def jointNamesToIds(names, model):
     return [i for (i, n) in enumerate(model.names) if n in names]
-
 
 def robexLoadAndReduce(urdfName, jointToLock_key):
     """Load a robot model from example robot data, using the urdfName
