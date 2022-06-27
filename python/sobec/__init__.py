56a6621ce65c17822a877feb92f967b97ecf9858
Nicolas Mansard <nmansard@laas.fr>
2022-06-27 12:17:53 +0200

    Remove import from __init__ following GSaurel suggestion
    Co-authored-by: Guilhem Saurel <guilhem.saurel@laas.fr>

--- python/sobec/__init__.py
+++ python/sobec/__init__.py
@@ -32,6 +32,4 @@ from .repr_ocp import reprProblem
 from .viewer_multiple import GepettoGhostViewer
 from . import logs
 
-# TODO discuss with Guilhem if it is a good it to alias this.
 from . import walk_without_think as wwt
-from . import walk_without_think
