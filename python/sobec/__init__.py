d59344dca2325a1639ffb19eef27c3485ab6e0eb
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-06-28 10:45:53 +0200

    Revert "Remove import from __init__ following GSaurel suggestion"
    wtf ?
    
    This reverts commit 56a6621ce65c17822a877feb92f967b97ecf9858.

--- python/sobec/__init__.py
+++ python/sobec/__init__.py
@@ -32,4 +32,6 @@ from .repr_ocp import reprProblem
 from .viewer_multiple import GepettoGhostViewer
 from . import logs
 
+# TODO discuss with Guilhem if it is a good it to alias this.
 from . import walk_without_think as wwt
+from . import walk_without_think
