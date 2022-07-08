55165fd8e734a386dc45d8f0cee9f4c0c8fbfd49
ManifoldFR <wilson.jallet@polytechnique.org>
2022-07-08 14:49:25 +0200

    Fixes for more recent version of Boost + Pinocchio3x
    + using use new containers from new eigenpy 2.8.0

--- python/std-containers.cpp
+++ python/std-containers.cpp
@@ -6,45 +6,51 @@
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <pinocchio/bindings/python/utils/std-map.hpp>
-#include <pinocchio/bindings/python/utils/std-vector.hpp>
-#include <pinocchio/fwd.hpp>
 #include <pinocchio/multibody/fwd.hpp>
 #include <pinocchio/spatial/force.hpp>
+#include <eigenpy/fwd.hpp>
+
+#if EIGENPY_VERSION_AT_LEAST(2,8,0)
+  #include <eigenpy/std-map.hpp>
+  #include <eigenpy/std-vector.hpp>
+#else
+  #include <pinocchio/bindings/python/utils/std-map.hpp>
+  #include <pinocchio/bindings/python/utils/std-vector.hpp>
+#endif
 
 namespace sobec {
 namespace python {
 
 namespace bp = boost::python;
-namespace pp = pinocchio::python;
+
+#if EIGENPY_VERSION_AT_LEAST(2,8,0)
+  using eigenpy::StdVectorPythonVisitor;
+#else
+  using pinocchio::python::StdVectorPythonVisitor;
+#endif
 
 void exposeStdContainers() {
-  pp::StdVectorPythonVisitor<pinocchio::FrameIndex>::expose(
-      "StdVectorPinocchioFrameIndex_");
+  StdVectorPythonVisitor<std::vector<std::string>>::expose(
+      "StdVectorStdStringIndex_");
 
-  pp::StdVectorPythonVisitor<std::string>::expose("StdVectorStdStringIndex_");
-
-  pp::StdVectorPythonVisitor<pinocchio::Force>::expose(
+  StdVectorPythonVisitor<std::vector<pinocchio::Force>>::expose(
       "StdVectorPinocchioForce_");
-  pp::StdVectorPythonVisitor<std::vector<pinocchio::Force>>::expose(
-      "StdVectorStdVectorPinocchioForce_");
+  StdVectorPythonVisitor<std::vector<std::vector<pinocchio::Force>>>::
+      expose("StdVectorStdVectorPinocchioForce_");
 
-  bp::class_<std::map<pinocchio::FrameIndex, pinocchio::FrameIndex>>(
+  using frame_frame_map_t = std::map<pinocchio::FrameIndex, pinocchio::FrameIndex>;
+  bp::class_<frame_frame_map_t>(
       "StdMapPinocchioFrameIndexToPinocchioFrameIndex_")
-      .def(bp::map_indexing_suite<
-           std::map<pinocchio::FrameIndex, pinocchio::FrameIndex>, true>())
-      // .def(pp::details::overload_base_get_item_for_std_map<std::map<pinocchio::FrameIndex,
+      .def(bp::map_indexing_suite<frame_frame_map_t, true>())
+      // .def(ep::details::overload_base_get_item_for_std_map<std::map<pinocchio::FrameIndex,
       // pinocchio::FrameIndex>>())
       ;
 
-  bp::class_<
-      std::pair<std::vector<Eigen::VectorXd>, std::vector<Eigen::VectorXd>>>(
-      "EigenVectorXd")
-      .def_readwrite("first", &std::pair<std::vector<Eigen::VectorXd>,
-                                         std::vector<Eigen::VectorXd>>::first)
-      .def_readwrite("second",
-                     &std::pair<std::vector<Eigen::VectorXd>,
-                                std::vector<Eigen::VectorXd>>::second);
+  using StdVecVectorXd = std::vector<Eigen::VectorXd>;
+  using pair_vec_vec_t = std::pair<StdVecVectorXd, StdVecVectorXd>;
+  bp::class_<pair_vec_vec_t>("StdPair_StdVector_EigenVectorXd")
+      .def_readwrite("first", &pair_vec_vec_t::first)
+      .def_readwrite("second", &pair_vec_vec_t::second);
 }
 
 }  // namespace python
