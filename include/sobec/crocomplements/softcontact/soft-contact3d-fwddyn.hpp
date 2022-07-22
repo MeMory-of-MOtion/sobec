3d0b022d6787b98050fe2be4bdaa4027ffb11835
Sébastien Kleff <sk8001@nyu.edu>
2022-07-22 17:22:46 +0200

    started to implement soft contact dam 3D

---
+++ include/sobec/crocomplements/softcontact/soft-contact3d-fwddyn.hpp
@@ -0,0 +1,214 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2019-2021, LAAS-CNRS, University of Edinburgh, CTU, INRIA,
+// University of Oxford Copyright note valid unless otherwise stated in
+// individual files. All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOBEC_SOFTCONTACT3D_FWDDYN_HPP_
+#define SOBEC_SOFTCONTACT3D_FWDDYN_HPP_
+
+#include <stdexcept>
+
+#include "crocoddyl/core/actuation-base.hpp"
+#include "crocoddyl/core/costs/cost-sum.hpp"
+#include "crocoddyl/core/diff-action-base.hpp"
+#include "crocoddyl/multibody/fwd.hpp"
+#include "crocoddyl/multibody/states/multibody.hpp"
+#include "crocoddyl/multibody/actions/free-fwddyn.hpp"
+
+// #include "sobec/crocomplements/contact/contact-fwddyn.hpp"
+#include "sobec/fwd.hpp"
+
+namespace sobec {
+
+/**
+ * @brief Differential action model for visco-elastic contact forward dynamics in multibody
+ * systems.
+ *
+ * Maths here : https://www.overleaf.com/read/xdpymjfhqqhn
+ *
+ * \sa `DifferentialActionModelAbstractTpl`, `calc()`, `calcDiff()`,
+ * `createData()`
+ */
+template <typename _Scalar>
+class DifferentialActionModelSoftContact3DFwdDynamicsTpl
+    : public crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<_Scalar> {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  typedef _Scalar Scalar;
+  typedef crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<Scalar> Base;
+  typedef crocoddyl::DifferentialActionDataFreeFwdDynamicsTpl<Scalar> Data;
+  typedef crocoddyl::MathBaseTpl<Scalar> MathBase;
+  typedef crocoddyl::CostModelSumTpl<Scalar> CostModelSum;
+  typedef crocoddyl::StateMultibodyTpl<Scalar> StateMultibody;
+  typedef crocoddyl::ActuationModelAbstractTpl<Scalar> ActuationModelAbstract;
+  typedef crocoddyl::DifferentialActionDataAbstractTpl<Scalar> DifferentialActionDataAbstract;
+  typedef typename MathBase::VectorXs VectorXs;
+  typedef typename MathBase::Vector3s Vector3s;
+  typedef typename MathBase::MatrixXs MatrixXs;
+
+  /**
+   * @brief Initialize the soft contact forward-dynamics action model
+   *
+   * It describes the dynamics evolution of a multibody system under
+   * visco-elastic contact (linear spring damper force)
+   *
+   * @param[in] state            State of the multibody system
+   * @param[in] actuation        Actuation model
+   * @param[in] costs            Stack of cost functions
+   * @param[in] frameId          Pinocchio frame id of the frame in contact
+   * @param[in] Kp               Soft contact model stiffness
+   * @param[in] Kv               Soft contact model damping
+   * @param[in] oPc              Anchor point of the contact model in WORLD coordinates
+   * @param[in] ref              Pinocchio reference frame in which the contact force is to be expressed
+   * 
+   */
+  DifferentialActionModelFreeFwdDynamicsTpl(
+      boost::shared_ptr<StateMultibody> state,
+      boost::shared_ptr<ActuationModelAbstract> actuation,
+      boost::shared_ptr<CostModelSum> costs,
+      const pinocchio::FrameIndex frameId,
+      const double Kp, 
+      const double Kv,
+      const Vector3s& oPc,
+      const pinocchio::ReferenceFrame ref = pinocchio::LOCAL);
+  virtual ~DifferentialActionModelFreeFwdDynamicsTpl();
+
+  /**
+   * @brief Compute the system acceleration, and cost value
+   *
+   * It computes the system acceleration using the free forward-dynamics.
+   *
+   * @param[in] data  Free forward-dynamics data
+   * @param[in] x     State point \f$\mathbf{x}\in\mathbb{R}^{ndx}\f$
+   * @param[in] u     Control input \f$\mathbf{u}\in\mathbb{R}^{nu}\f$
+   */
+  virtual void calc(const boost::shared_ptr<DifferentialActionDataAbstract>& data, const Eigen::Ref<const VectorXs>& x,
+                    const Eigen::Ref<const VectorXs>& u);
+
+  /**
+   * @brief Compute the derivatives of the contact dynamics, and cost function
+   *
+   * @param[in] data  Contact forward-dynamics data
+   * @param[in] x     State point \f$\mathbf{x}\in\mathbb{R}^{ndx}\f$
+   * @param[in] u     Control input \f$\mathbf{u}\in\mathbb{R}^{nu}\f$
+   */
+  virtual void calcDiff(
+      const boost::shared_ptr<DifferentialActionDataAbstract>& data,
+      const Eigen::Ref<const VectorXs>& x, const Eigen::Ref<const VectorXs>& u);
+  
+  void set_force_cost(const Vector3s& force_des, const Scalar force_weight);
+
+  protected:
+    Scalar Kp_; //!< Contact model stiffness
+    Scalar Kv_;
+    Vector3s oPc_;
+    pinocchio::FrameIndex frameId_;
+    pinocchio::ReferenceFrame ref_;
+    bool with_force_cost_;
+    bool active_contact_;
+    std::size_t nc_;
+    Vector3s force_des_;
+    Scalar force_weight_;
+    pinocchio::SE3Tpl<Scalar> jMf_;
+};
+
+template <typename _Scalar>
+struct DifferentialActionDataSoftContact3DFwdDynamicsTpl : public DifferentialActionDataFreeFwdDynamicsTpl<_Scalar> {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  typedef _Scalar Scalar;
+  typedef MathBaseTpl<Scalar> MathBase;
+  typedef DifferentialActionDataAbstractTpl<Scalar> Base;
+  typedef typename MathBase::VectorXs VectorXs;
+  typedef typename MathBase::Vector3s Vector3s;
+  typedef typename MathBase::MatrixXs MatrixXs;
+  typedef typename MathBase::Matrix3s Matrix3s;
+
+  template <template <typename Scalar> class Model>
+  explicit DifferentialActionDataSoftContact3DFwdDynamicsTpl(Model<Scalar>* const model)
+      : Base(model),
+        pinocchio(pinocchio::DataTpl<Scalar>(model->get_pinocchio())),
+        multibody(&pinocchio, model->get_actuation()->createData()),
+        costs(model->get_costs()->createData(&multibody)),
+        Minv(model->get_state()->get_nv(), model->get_state()->get_nv()),
+        u_drift(model->get_nu()),
+        dtau_dx(model->get_nu(), model->get_state()->get_ndx()),
+        tmp_xstatic(model->get_state()->get_nx()),
+        lJ(6, model->get_state()->get_nv()),
+        oJ(6, model->get_state()->get_nv()),
+        lv_partial_dq(6, model->get_state()->get_ndx()),
+        lv_partial_dv(6, model->get_state()->get_ndx()),
+        aba_dq(model->get_state()->get_nv(), model->get_state()->get_ndx()),
+        aba_dv(model->get_state()->get_nv(), model->get_state()->get_ndx()),
+        aba_dtau(model->get_state()->get_nv(), model->get_state()->get_ndx()),
+        df_dx(model->get_nc(), model->get_state()->get_ndx()),
+        df_dx_copy(model->get_nc(), model->get_state()->get_ndx()),
+        // f(model->get_nc(), model->get_state()->get_ndx()),
+        // f_copy(model->get_nc(), model->get_state()->get_ndx()),
+        pinForce(pinocchio::ForceTpl<Scalar>::Zero()),
+        fext(model->get_state()->get_pinocchio()->njoints, pinocchio::ForceTpl<Scalar>::Zero()),
+        fext_copy(model->get_state()->get_pinocchio()->njoints, pinocchio::ForceTpl<Scalar>::Zero()) {
+    costs->shareMemory(this);
+    Minv.setZero();
+    u_drift.setZero();
+    dtau_dx.setZero();
+    tmp_xstatic.setZero();
+    df_dx.setZero();
+    df_dx_copy.setZero();
+    f.setZero();
+    f_copy.setZero();
+    oRf.setZero();
+    lv.setZero();
+    f_residual.setZero();
+  }
+
+  pinocchio::DataTpl<Scalar> pinocchio;
+  DataCollectorActMultibodyTpl<Scalar> multibody;
+  boost::shared_ptr<CostDataSumTpl<Scalar> > costs;
+  MatrixXs Minv;
+  VectorXs u_drift;
+  MatrixXs dtau_dx;
+  VectorXs tmp_xstatic;
+
+  Matrix3s oRf;
+  Vector3s lv;
+  MatrixXs d->lJ;
+  MatrixXs d->oJ;
+  MatrixXs lv_partial_dq;
+  MatrixXs lv_partial_dv;
+  MatrixXs aba_dq;
+  MatrixXs aba_dv;
+  MatrixXs aba_dtau;
+  // force cost & derivatives
+  Vector3d f_residual;
+  MatrixXs df_dx; 
+  MatrixXs df_dx_copy;
+  Vector3s f;
+  Vector3s f_copy;
+  pinocchio::ForceTpl<Scalar> pinForce;
+  pinocchio::container::aligned_vector<pinocchio::ForceTpl<Scalar> > fext;  //!< External spatial forces in body coordinates (joint level)
+  pinocchio::container::aligned_vector<pinocchio::ForceTpl<Scalar> > fext_copy;  //!< External spatial forces in body coordinates (joint level)
+
+  using Base::cost;
+  using Base::Fu;
+  using Base::Fx;
+  using Base::Lu;
+  using Base::Luu;
+  using Base::Lx;
+  using Base::Lxu;
+  using Base::Lxx;
+  using Base::r;
+  using Base::xout;
+};
+
+}  // namespace sobec
+
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+/* --- Details -------------------------------------------------------------- */
+#include <sobec/crocomplements/contact/softcontact3d-fwddyn.hxx>
+
+#endif  // SOBEC_SOFTCONTACT3D_FWDDYN_HPP_
---
+++ include/sobec/crocomplements/softcontact/soft-contact3d-fwddyn.hxx
@@ -0,0 +1,262 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+//
+// Copyright (C) 2019-2020, LAAS-CNRS, University of Edinburgh
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#include <crocoddyl/core/utils/exception.hpp>
+#include <crocoddyl/core/utils/math.hpp>
+#include <pinocchio/algorithm/centroidal.hpp>
+#include <pinocchio/algorithm/compute-all-terms.hpp>
+#include <pinocchio/algorithm/contact-dynamics.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/kinematics-derivatives.hpp>
+#include <pinocchio/algorithm/rnea-derivatives.hpp>
+#include <pinocchio/algorithm/rnea.hpp>
+
+#include "soft-contact3d-fwddyn.hpp"
+
+namespace crocoddyl {
+
+template <typename Scalar>
+DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::DifferentialActionModelSoftContact3DFwdDynamicsTpl(
+    boost::shared_ptr<StateMultibody> state, 
+    boost::shared_ptr<ActuationModelAbstract> actuation,
+    boost::shared_ptr<CostModelSum> costs,
+    const pinocchio::FrameIndex frameId,
+    const double Kp, 
+    const double Kv,
+    const Vector3s& oPc,
+    const pinocchio::ReferenceFrame ref)
+    : Base(state, actuation->get_nu(), costs->get_nr()),
+      actuation_(actuation),
+      costs_(costs),
+      pinocchio_(*state->get_pinocchio().get()),
+      without_armature_(true),
+      armature_(VectorXs::Zero(state->get_nv())) {
+  if (costs_->get_nu() != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "Costs doesn't have the same control dimension (it should be " + std::to_string(nu_) + ")");
+  }
+  Base::set_u_lb(Scalar(-1.) * pinocchio_.effortLimit.tail(nu_));
+  Base::set_u_ub(Scalar(+1.) * pinocchio_.effortLimit.tail(nu_));
+  // Soft contact model parameters
+  if(Kp < 0.){
+     throw_pretty("Invalid argument: "
+                << "Kp must be positive "); 
+  }
+  if(Kv < 0.){
+     throw_pretty("Invalid argument: "
+                << "Kv must be positive "); 
+  }
+  Kp_ = Kp;
+  Kv_ = Kv;
+  oPc_ = oPc;
+  frameId_ = frameId;
+  ref_ = ref;
+  with_force_cost_ = false;
+  active_contact_ = true;
+  nc_ = 3;
+  parentId_ = pinocchio_.frames[frameId_].parent();
+  jMf_ = pinocchio_.frames[frameId_].placement();
+}
+
+template <typename Scalar>
+DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::~DifferentialActionModelSoftContact3DFwdDynamicsTpl() {}
+
+template <typename Scalar>
+void DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::calc(
+            const boost::shared_ptr<DifferentialActionDataAbstract>& data, 
+            const Eigen::Ref<const VectorXs>& x,
+            const Eigen::Ref<const VectorXs>& u) {
+  if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
+    throw_pretty("Invalid argument: "
+                 << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
+  }
+  if (static_cast<std::size_t>(u.size()) != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
+  }
+
+  Data* d = static_cast<Data*>(d->get());
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(state_->get_nq());
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> v = x.tail(state_->get_nv());
+  d->oRf = d->pinocchio.oMf[frameId_].rotation();
+
+  // Actuation calc
+  actuation_->calc(d->multibody.actuation, x, u);
+  
+  // If contact is active, compute aq = ABA(q,v,tau,fext)
+  if(active_contact_){
+    // Computing the dynamics using ABA or manually for armature case
+    // if (without_armature_) {
+    // Compute spring damper force expressed at joint level 
+    d->lv = pinocchio::getFrameVelocity(pinocchio_, d->pinocchio, frameId_, pinocchio::LOCAL).linear();
+    d->f = -Kp_ * d->oRf.transpose() * ( d->pinocchio.oMf[frameId_].translation() - oPc_ ) - Kv_*d->lv;
+    d->pinForce = pinocchio::ForceTpl<Scalar>(d->f, Vector3s::Zero());
+    d->fext[parentId_] = jMf_.act(d->pinForce);
+    // Save local force for later
+    d->f_copy = d->f;
+    d->fext_copy = d->fext;
+    // rotate if not local 
+    if(ref_ != pinocchio::LOCAL){
+        d->f = -Kp_ * ( d->pinocchio.oMf[frameId_].translation() - oPc_ ) - Kv_ * d->oRf * d->lv;
+        d->pinForce = pinocchio::ForceTpl<Scalar>(d->oRf.transpose() * d->f, Vector3s::Zero());
+        d->fext[parentId_] = jMf_.act(d->pinForce);
+    }
+    d->xout = pinocchio::aba(pinocchio_, d->pinocchio, q, v, d->multibody.actuation->tau, d->fext);
+    pinocchio::updateGlobalPlacements(pinocchio_, d->pinocchio);
+    // } 
+    // else {
+    //     pinocchio::computeAllTerms(pinocchio_, d->pinocchio, q, v);
+    //     d->pinocchio.M.diagonal() += armature_;
+    //     pinocchio::cholesky::decompose(pinocchio_, d->pinocchio);
+    //     d->Minv.setZero();
+    //     pinocchio::cholesky::computeMinv(pinocchio_, d->pinocchio, d->Minv);
+    //     d->u_drift = d->multibody.actuation->tau - d->pinocchio.nle;
+    //     d->xout.noalias() = d->Minv * d->u_drift;
+    // }
+  } 
+  // If contact NOT active : compute aq = ABA(q,v,tau)
+  else {
+    // Computing the dynamics using ABA or manually for armature case
+    if (without_armature_) {
+        d->xout = pinocchio::aba(pinocchio_, d->pinocchio, q, v, d->multibody.actuation->tau);
+        pinocchio::updateGlobalPlacements(pinocchio_, d->pinocchio);
+    } else {
+        pinocchio::computeAllTerms(pinocchio_, d->pinocchio, q, v);
+        d->pinocchio.M.diagonal() += armature_;
+        pinocchio::cholesky::decompose(pinocchio_, d->pinocchio);
+        d->Minv.setZero();
+        pinocchio::cholesky::computeMinv(pinocchio_, d->pinocchio, d->Minv);
+        d->u_drift = d->multibody.actuation->tau - d->pinocchio.nle;
+        d->xout.noalias() = d->Minv * d->u_drift;
+    }
+  }
+
+  // Computing the cost value and residuals
+  costs_->calc(d->costs, x, u);
+  d->cost = d->costs->cost;
+
+  // Add hard-coded cost on contact force
+  if(active_contact_ && with_force_cost_){
+    d->f_residual = d->f - force_des_;
+    d->cost += force_weight_ * d->f_residual.transpose() * d->f_residual;
+  }
+}
+
+
+template <typename Scalar>
+void DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::calcDiff(
+    const boost::shared_ptr<DifferentialActionDataAbstract>& data, 
+    const Eigen::Ref<const VectorXs>& x,
+    const Eigen::Ref<const VectorXs>& u) {
+  if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
+    throw_pretty("Invalid argument: "
+                 << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
+  }
+  if (static_cast<std::size_t>(u.size()) != nu_) {
+    throw_pretty("Invalid argument: "
+                 << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
+  }
+
+  const std::size_t nv = state_->get_nv();
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> q = x.head(state_->get_nq());
+  const Eigen::VectorBlock<const Eigen::Ref<const VectorXs>, Eigen::Dynamic> v = x.tail(nv);
+  d->oRf = d->pinocchio.oMf[frameId_].rotation();
+  
+  Data* d = static_cast<Data*>(data.get());
+  
+  // Actuation calcDiff
+  actuation_->calcDiff(d->multibody.actuation, x, u);
+  
+  // If contact is active, compute ABA derivatives + force
+  if(active_contact_){
+    // Compute spring damper force derivatives in LOCAL
+    d->lJ = pinocchio::getFrameJacobian(pinocchio_, d->pinocchio, frameId_, pinocchio::LOCAL);
+    d->oJ = pinocchio::getFrameJacobian(pinocchio_, d->pinocchio, frameId_, pinocchio::LOCAL_WORLD_ALIGNED);
+    d->lv_partial_dq, d->lv_partial_dv = pinocchio::getFrameVelocityDerivatives(pinocchio_, d->pinocchio, frameId_, pinocchio::LOCAL);
+    d->df_dx.leftCols(nv) = 
+        -Kp_ * (d->lJ.topRows(3) + pinocchio::skew(d->oRf.transpose() * (d->pinocchio.oMf[frameId_].translation() - oPc_)) * d->lJ.bottomRows(3)) - Kv_* d->lv_partial_dq.topRows(3);
+    d->df_dx.rightCols(nv) = 
+        -Kv_ * d->lv_partial_dv.topRows();
+    // copy for later
+    d->df_dx_copy = d->df_dx;
+    // rotate force derivatives if not local 
+    if(ref_ != pinocchio::LOCAL):
+        d->df_dx.leftCols(nv) = d->oRf * d->df_dx_copy.leftCols(nv) - pinocchio::skew(d->oRf * d->f_copy) * oJ.bottomRows(3);
+        d->df_dx.rightCols(nv) = d->oRf * d->df_dx_copy.rightCols(nv);
+    // Compute ABA derivatives (same in LOCAL and LWA for 3D contact)
+    d->aba_dq, d->aba_dv, d->aba_dtau = pinocchio::computeABADerivatives(pinocchio_, d->pinocchio, q, v, d->multibody.actuation.tau, d->fext_copy);
+    d->Fx.leftCols(nv) = d->aba_dq + d->pinocchio.Minv * d->lJ.topRows(3).transpose() * d->df_dx_copy.leftCols(nv);
+    d->Fx.rightCols(nv) = d->aba_dv + d->pinocchio.Minv * d->lJ.topRows(3).transpose() * d->df_dx_copy.rightCols(nv);
+    d->Fx += d->pinocchio.Minv * d->multibody.actuation.dtau_dx;
+    d->Fu = d->aba_dtau * d->multibody.actuation.dtau_du;
+  }
+
+  // Else ABA Derivatives
+  else{
+    // Computing the dynamics derivatives
+    if (without_armature_) {
+        pinocchio::computeABADerivatives(pinocchio_, d->pinocchio, q, v, d->multibody.actuation->tau, d->Fx.leftCols(nv),
+                                        d->Fx.rightCols(nv), d->pinocchio.Minv);
+        d->Fx.noalias() += d->pinocchio.Minv * d->multibody.actuation->dtau_dx;
+        d->Fu.noalias() = d->pinocchio.Minv * d->multibody.actuation->dtau_du;
+    } else {
+        pinocchio::computeRNEADerivatives(pinocchio_, d->pinocchio, q, v, d->xout);
+        d->dtau_dx.leftCols(nv) = d->multibody.actuation->dtau_dx.leftCols(nv) - d->pinocchio.dtau_dq;
+        d->dtau_dx.rightCols(nv) = d->multibody.actuation->dtau_dx.rightCols(nv) - d->pinocchio.dtau_dv;
+        d->Fx.noalias() = d->Minv * d->dtau_dx;
+        d->Fu.noalias() = d->Minv * d->multibody.actuation->dtau_du;
+    }
+  }
+
+
+  // Computing the cost derivatives
+  costs_->calcDiff(d->costs, x, u);
+  
+  // Add costs on force 
+  d->Lx = d->costs->Lx;
+  d->Lu = d->costs->Lu;
+  d->Lxx = d->costs->Lxx;
+  d->Lxu = d->costs->Lxu;
+  d->Luu = d->costs->Luu;
+  if(active_contact_ && with_force_cost_):
+      d->f_residual = d->f - force_des_;
+      d->Lx += force_weight_ * d->f_residual.transpose() * d->df_dx;
+      d->Lxx += force_weight_ * d->df_dx.transpose() * d->df_dx;
+}
+
+template <typename Scalar>
+void DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::calcDiff(
+    const boost::shared_ptr<DifferentialActionDataAbstract>& data, const Eigen::Ref<const VectorXs>& x) {
+  if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
+    throw_pretty("Invalid argument: "
+                 << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
+  }
+  Data* d = static_cast<Data*>(data.get());
+
+  costs_->calcDiff(d->costs, x);
+}
+
+template <typename Scalar>
+boost::shared_ptr<DifferentialActionDataAbstractTpl<Scalar> >
+DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::createData() {
+  return boost::allocate_shared<Data>(Eigen::aligned_allocator<Data>(), this);
+}
+
+template <typename Scalar>
+void DifferentialActionModelSoftContact3DFwdDynamicsTpl<Scalar>::set_force_cost(const Vector3s& force_des, 
+                                                                                const Scalar force_weight) {
+  if (force_weight < 0.) {
+    throw_pretty("Invalid argument: "
+                 << "Force weight should be positive");
+  }
+  force_des_ = force_des;
+  force_weight_ = force_weight;
+  with_force_cost_ = true;
+}
+
+}  // namespace crocoddyl
