c98e2385580038eecf955c5a3c91e4ad4b1149b1
Ewen Dantec <edantec@eboshisan>
2022-10-03 18:50:28 +0200

    Bug in getActuationRef

--- src/walk-with-traj/horizon_manager.cpp
+++ src/walk-with-traj/horizon_manager.cpp
@@ -152,9 +152,9 @@ void HorizonManager::setActuationReference(const unsigned long time,
 
 Eigen::VectorXd HorizonManager::getActuationReference(const unsigned long time,
                                            const std::string &nameCostActuation) {
-  return boost::static_pointer_cast<crocoddyl::ResidualModelState>(
-          costs(time)->get_costs().at(nameCostState)->cost->get_residual())
-          ->get_reference();
+  return boost::static_pointer_cast<crocoddyl::ResidualModelControl>(
+      costs(time)->get_costs().at(nameCostActuation)->cost->get_residual())
+      ->get_reference();
 }
 
 void HorizonManager::setPoseReference(const unsigned long time,
