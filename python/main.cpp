20cb09f18459fd60b6fcc6d6f7338b8876fcc8c0
ManifoldFR <wilson.jallet@polytechnique.org>
2022-11-11 18:45:03 +0100

    python/main.cpp: use namespace aliases
    * there should be no conflict in this file
    * use properly namespace'd eigenpy::enableEigenPySpecific function instead of macro

--- python/main.cpp
+++ python/main.cpp
@@ -3,11 +3,13 @@
 #include "sobec/python.hpp"
 
 BOOST_PYTHON_MODULE(sobec_pywrap) {
-  boost::python::import("pinocchio");
-  boost::python::import("crocoddyl");
+  namespace bp = boost::python;
+
+  bp::import("pinocchio");
+  bp::import("crocoddyl");
   // Enabling eigenpy support, i.e. numpy/eigen compatibility.
   eigenpy::enableEigenPy();
-  ENABLE_SPECIFIC_MATRIX_TYPE(Eigen::VectorXi);
+  eigenpy::enableEigenPySpecific<Eigen::VectorXi>();
   sobec::python::exposeStdContainers();
   sobec::python::exposeResidualVelCollision();
   sobec::python::exposeResidualCoMVelocity();
