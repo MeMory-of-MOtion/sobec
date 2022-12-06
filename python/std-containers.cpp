57fd7f607ff4f9d3a8a4da5620a30fe32a545f11
ManifoldFR <wilson.jallet@polytechnique.org>
2022-12-06 14:15:34 +0100

    python/std-containers: fix pinocchio<2.9.2, eigenpy<2.8

--- python/std-containers.cpp
+++ python/std-containers.cpp
@@ -6,16 +6,16 @@
 // All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <eigenpy/fwd.hpp>
 #include <pinocchio/multibody/fwd.hpp>
 #include <pinocchio/spatial/force.hpp>
-#include <eigenpy/fwd.hpp>
 
-#if EIGENPY_VERSION_AT_LEAST(2,8,0)
-  #include <eigenpy/std-map.hpp>
-  #include <eigenpy/std-vector.hpp>
+#if EIGENPY_VERSION_AT_LEAST(2, 8, 0)
+#include <eigenpy/std-map.hpp>
+#include <eigenpy/std-vector.hpp>
 #else
-  #include <pinocchio/bindings/python/utils/std-map.hpp>
-  #include <pinocchio/bindings/python/utils/std-vector.hpp>
+#include <pinocchio/bindings/python/utils/std-map.hpp>
+#include <pinocchio/bindings/python/utils/std-vector.hpp>
 #endif
 
 namespace sobec {
@@ -23,10 +23,15 @@ namespace python {
 
 namespace bp = boost::python;
 
-#if EIGENPY_VERSION_AT_LEAST(2,8,0)
-  using eigenpy::StdVectorPythonVisitor;
+#if EIGENPY_VERSION_AT_LEAST(2, 8, 0)
+using eigenpy::StdVectorPythonVisitor;
+#elif PINOCCHIO_VERSION_AT_LEAST(2, 9, 2)
+using pinocchio::python::StdVectorPythonVisitor;
 #else
-  using pinocchio::python::StdVectorPythonVisitor;
+// the template parameter used to be the value_type.
+template <typename vector_type>
+struct StdVectorPythonVisitor : pinocchio::python::StdVectorPythonVisitor<
+                                    typename vector_type::value_type> {};
 #endif
 
 void exposeStdContainers() {
@@ -35,10 +40,11 @@ void exposeStdContainers() {
 
   StdVectorPythonVisitor<std::vector<pinocchio::Force>>::expose(
       "StdVectorPinocchioForce_");
-  StdVectorPythonVisitor<std::vector<std::vector<pinocchio::Force>>>::
-      expose("StdVectorStdVectorPinocchioForce_");
+  StdVectorPythonVisitor<std::vector<std::vector<pinocchio::Force>>>::expose(
+      "StdVectorStdVectorPinocchioForce_");
 
-  using frame_frame_map_t = std::map<pinocchio::FrameIndex, pinocchio::FrameIndex>;
+  using frame_frame_map_t =
+      std::map<pinocchio::FrameIndex, pinocchio::FrameIndex>;
   bp::class_<frame_frame_map_t>(
       "StdMapPinocchioFrameIndexToPinocchioFrameIndex_")
       .def(bp::map_indexing_suite<frame_frame_map_t, true>())
